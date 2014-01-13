6067ad2e04ec6ebb4b6210f028442169369d0167
Steven Joseph <steven@stevenjoseph.in>
2014-01-13 11:29:57 +1100

    added launcher script

---
+++ bin/launch.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Script to setup env and run a development server for app
+Does the following:
+- setup sys path to libraries included
+- change dir to src
+- execvpe sys.argv[1] with parameters passed and default environ
+"""
+import os
+from os.path import join, dirname, abspath
+import sys
+import site
+paths = []
+
+base_path = abspath(join(dirname(__file__), ".."))
+src_path = join(base_path, "src")
+lib_path = join(base_path, "lib")
+paths.append(src_path)
+sys.path.extend(paths)
+site.addsitedir(lib_path)
+os.chdir(src_path)
+os.execvp("python3", sys.argv[1:])#, os.environ)
