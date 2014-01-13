46c90e5f7bbbcfccecb48df86d7fbf3de3acafdf
Steven Joseph <steven@stevenjoseph.in>
2014-01-13 11:07:01 +1100

    added util.py

---
+++ src/aioweb/util.py
@@ -0,0 +1,25 @@
+import logging
+import sys
+from IPython.core import ultratb
+
+
+class AioWebLogFormatter(logging.Formatter):
+    """ Pretty formatter for logging
+    http://nbviewer.ipython.org/gist/minrk/8330041
+    """
+    def __init__(self, *args, **kwargs):
+        super(AioWebLogFormatter, self).__init__(*args, **kwargs)
+        self.ftb = ultratb.FormattedTB()
+
+    def formatException(self, exc_info):
+        etype, evalue, etb = exc_info
+        tblist = self.ftb.structured_traceback(etype, evalue, etb)
+        return '\n'.join(tblist)
+
+
+def configure_logging(color_console=True):
+    root = logging.getLogger()
+    if color_console:
+        for handler in root.handlers:
+            if isinstance(handler, logging.StreamHandler):
+                handler.setFormatter(AioWebLogFormatter())
